38a64cc9a0c5ea95dd58ee2abd690645be2c3be5
Yuratama1 <yuratama661@gmail.com>
2025-06-05 14:54:59 +0700

    stak penuh print massage dan ulangi

--- Stack.cpp
+++ Stack.cpp
@@ -38,6 +38,9 @@ class stack{
 
     //Pop operation : remove the topmost element from the stack
     void pop(){
-        
+        if (isEmpty()){
+            cout << "Stack is empty." << endl;
+            return; //if the stack is empty, print a masage and return
+        }
     }
 };
