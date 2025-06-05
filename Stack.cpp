f25d6f4ab844407ef68118b30b167861f3a8fce3
Yuratama1 <yuratama661@gmail.com>
2025-06-05 15:07:39 +0700

    memebuat menu pilihan

--- Stack.cpp
+++ Stack.cpp
@@ -66,4 +66,15 @@ int main(){
 
     int choice = 0;
     int value;
+
+    while(choice != 4){
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "$. Exit\n";
+        cout << "Enter your choice:";
+        cin >> choice;
+
+        switch
+    }
 }
