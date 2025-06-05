6e83da5300984a904c33e8b6f2321f4cf56f6395
Yuratama1 <yuratama661@gmail.com>
2025-06-05 15:11:25 +0700

    push the entered value onto the stack

--- Stack.cpp
+++ Stack.cpp
@@ -75,6 +75,14 @@ int main(){
         cout << "Enter your choice:";
         cin >> choice;
 
-        switch
+        switch(choice){
+            case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value); //push the entered value onto the stack
+                break;
+            
+
+        }
     }
 }
